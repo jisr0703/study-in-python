e9be4e26c94298e24df0f4f9c534dc66a81661e0
seungryul <sungryul@stanworld.kr>
2022-10-05 14:47:28 +0900

    백준 3273번 완료 & 구조 변경

---
+++ app/app/1. Array/1.3 두 수의 합 찾기/백준-3273번-두_수의_합.py
@@ -0,0 +1,28 @@
+import sys
+from typing import List
+
+# N: int = int(input())
+# NUMS: List[int] = sorted(list(map(int, sys.stdin.readline().split())))
+# X: int = int(input())
+N = 9
+NUMS = sorted( [5, 12, 7, 10, 9, 1, 2, 3, 11])
+X = 13
+
+
+def two_sum(n, nums, x):
+    l, r = 0, n - 1
+    cnt: int = 0
+    while l < r:
+        sum = nums[l] + nums[r]
+        if x == sum:
+            cnt += 1
+            l += 1
+            r -= 1
+        elif x > sum:
+            l += 1
+        else:
+            r -= 1
+    return cnt
+
+
+print(two_sum(N, NUMS, X))
